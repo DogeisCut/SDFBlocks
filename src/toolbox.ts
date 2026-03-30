df0b6032ed483d7fa89cd72534f6c7f33d9da854
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-03-30 17:50:25 -0600

    add stubs to toolbox

--- src/toolbox.ts
+++ src/toolbox.ts
@@ -27,10 +27,12 @@ function shadowColor(color: string = "#ff0000"): string {
 const c = theme.blockStyles
 const toolbox = `
     <category name="Scene" colour="${c.scene_blocks.colourPrimary}">
-        ${block("scene_current_scene")}
         ${block("scene_set_scene", value("SDF", shadow("values_sdf")))}
         ${block("scene_append_to_scene", value("SDF", shadow("values_sdf")))}
+        ${block("scene_current_scene")}
         ${sep(50)}
+        ${block("scene_customize_shadows")}
+        ${block("scene_customize_shading")}
         ${block("scene_set_clear_color", value("COLOR", shadowColor("#000000")))}
         ${block("scene_set_fog", value("MIN", shadowFloat(100)), value("MAX", shadowFloat(1000)))}
     </category>
@@ -43,22 +45,47 @@ const toolbox = `
     <category name="Primatives" colour="${c.primatives_blocks.colourPrimary}">
         ${block("primatives_nothing")}
         ${block("primatives_sphere", value("SURFACE", shadow("values_surface")), value("RADIUS", shadowFloat(1)))}
+        ${block("primatives_plane")}
+        ${block("primatives_box")}
+        ${block("primatives_round_box")}
+        ${block("primatives_cone")}
+        ${block("primatives_torus")}
+        ${block("primatives_capsule")}
+        ${block("primatives_capped_cylinder")}
+        ${block("primatives_rounded_cylinder")}
+        ${block("primatives_round_cone")}
+        ${block("primatives_ellipsoid")}
     </category>
 
     <category name="Combiners" colour="${c.combiners_blocks.colourPrimary}">
         ${block("combiners_union", value("A", shadow("values_sdf")), value("B", shadow("values_sdf")))}
         ${block("combiners_smooth_union", value("A", shadow("values_sdf")), value("B", shadow("values_sdf")), value("SMOOTHNESS", shadowFloat(5)))}
+        ${block("combiners_subtract")}
+        ${block("combiners_smooth_subtract")}
+        ${block("combiners_intersect")}
+        ${block("combiners_smooth_intersect")}
+        ${block("combiners_paint")}
+        ${block("combiners_smooth_paint")}
     </category>
 
     <category name="Transforms" colour="${c.transforms_blocks.colourPrimary}">
         ${block("transfoms_current_transform")}
         ${block("transforms_translate", value("SDF", shadow("values_sdf")), value("POSITION", shadow("values_vector3")))}
+        ${block("transforms_rotate_around_axis")}
+        ${block("transforms_repeat")}
+        ${block("transforms_twist")}
+        ${block("transforms_bend")}
     </category>
 
     <category name="Color" colour="${c.color_blocks.colourPrimary}">
         ${block("color_r_g_b", value("R", shadowUnitFloat(1)), value("G", shadowUnitFloat(0)), value("B", shadowUnitFloat(0)))}
+        ${block("color_h_s_v")}
         ${sep(50)}
         ${block("color_blend", value("A", shadowColor()), value("B", shadowColor("#000000")), value("BY", shadowUnitFloat()))}
+        ${sep(50)}
+        ${block("color_get_r")}
+        ${block("color_get_g")}
+        ${block("color_get_b")}
     </category>
 
     <category name="Vectors" colour="${c.vectors_blocks.colourPrimary}">
@@ -70,11 +97,29 @@ const toolbox = `
         ${block("vectors_y_of", value("VECTOR", shadow("values_vector2")))}
         ${block("vectors_z_of", value("VECTOR", shadow("values_vector3")))}
         ${block("vectors_w_of", value("VECTOR", shadow("values_vector4")))}
+        ${sep(50)}
+        ${block("vectors_vector2_z")}
+        ${block("vectors_vector3_w")}
+        ${sep(50)}
+        ${block("vectors_components2")}
+        ${block("vectors_components3")}
+        ${block("vectors_components4")}
+        ${sep(50)}
+        ${block("vectors_length_of")}
     </category>
 
     <category name="Control" colour="${c.control_blocks.colourPrimary}">
         ${block("control_if")}
         ${block("control_if_else")}
+        ${sep(50)}
+        ${block("control_repeat")}
+        ${block("control_while")}
+        ${block("control_do_while")}
+        ${block("control_loop_index")}
+        ${sep(50)}
+        ${block("control_continue")}
+        ${block("control_break")}
+        ${block("control_discard")}
     </category>
 
     <category name="Operators" colour="${c.operators_blocks.colourPrimary}">
@@ -83,10 +128,46 @@ const toolbox = `
         ${block("operators_multiply", value("A", shadowFloat(0)), value("B", shadowFloat(0)))}
         ${block("operators_divide", value("A", shadowFloat(0)), value("B", shadowFloat(0)))}
         ${block("operators_power", value("A", shadowFloat(0)), value("B", shadowFloat(0)))}
+        ${block("operators_modulus")}
+        ${sep(50)}
+        ${block("operators_rounding")}
+        ${sep(50)}
+        ${block("operators_trig")}
+        ${sep(50)}
+        ${block("operators_unary")}
+        ${sep(50)}
+        ${block("operators_mix")}
+        ${sep(50)}
+        ${block("operators_bounds")}
+        ${block("operators_clamp")}
+        ${sep(50)}
+        ${block("operators_equals")}
+        ${block("operators_not_equals")}
+        ${block("operators_less_than")}
+        ${block("operators_less_than_or_equal")}
+        ${block("operators_greater_than")}
+        ${block("operators_greater_than_or_equal")}
+        ${sep(50)}
+        ${block("operators_and")}
+        ${block("operators_or")}
+        ${block("operators_not")}
+        ${sep(50)}
+        ${block("operators_true")}
+        ${block("operators_false")}
     </category>
 
     <category name="Sensing" colour="${c.sensing_blocks.colourPrimary}">
         ${block("sensing_timer")}
+        ${sep(50)}
+        ${block("sensing_set_camera_position")}
+        ${block("sensing_set_camera_angle")}
+        ${sep(50)}
+        ${block("sensing_camera_position")}
+        ${block("sensing_camera_angle")}
+        ${sep(50)}
+        ${block("sensing_resolution")}
+        ${sep(50)}
+        ${block("sensing_fragment_coord")}
     </category>
 
     <category name="Labeling" colour="${c.labeling_blocks.colourPrimary}">
