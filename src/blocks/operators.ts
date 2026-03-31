0343731b8b4f94237ab363b5ad159d96e154acf0
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-03-31 04:42:24 -0600

    Yeahhh not totally satisfied with this solution either

--- src/blocks/operators.ts
+++ src/blocks/operators.ts
@@ -1,5 +1,14 @@
 import * as Blockly from "blockly";
 import * as BlocklyGLSL from "../generators/glsl";
+import {
+    NUMERIC_TYPES,
+    NumericGLSLType,
+    compatiblePair,
+    resolveOutputType,
+    getConnectedType,
+    normalizeVec,
+    withAlias,
+} from "./helpers";
 
 /*
 I have a huge design problem that popped up in MarchBlocks, and I have no idea how to fix it.
@@ -78,75 +87,183 @@ Here's the choices im working between.
     - Cons:
         - Nobody is gonna want to use MarchBlocks if there's 50 different blocks just to do the same thing to different types.
         - Clutters toolbox.
-
+6. One and two combined, Basically, only A dictates what B can contain, instead of them dictating each other.
+    - Pros:
+        - Somewhat easy to implement.
+        - Intuative once understood
+        - Strictly typed, impossible to make an "invalid" combo
+    - Cons:
+        - May cause confusion why some things cant go in slot B when there's just numbers
+        - This has the issue of not being explained to the user at all.
+        - Would still need to figure out what is being outputted from all the slots.
 If someone can find a solution thats easy enough to implement, doesnt make the block bigger, and is intuative to the user, i'd be a very
 happy man.
 */
 
+type BinaryInputNames = [string, string];
+
+function makeSymmetricOnChange(inputs: BinaryInputNames): (this: Blockly.Block, e: Blockly.Events.Abstract) => void {
+    return function (e) {
+        if (
+            e.type !== Blockly.Events.BLOCK_MOVE &&
+            e.type !== Blockly.Events.BLOCK_DRAG
+        ) return;
+        if (this.isInFlyout) return;
+
+        const [nameA, nameB] = inputs;
+        const aType = getConnectedType(this, nameA);
+        const bType = getConnectedType(this, nameB);
+
+        this.getInput(nameA)?.setCheck(bType ? compatiblePair(bType) : NUMERIC_TYPES);
+        this.getInput(nameB)?.setCheck(aType ? compatiblePair(aType) : NUMERIC_TYPES);
+        this.setOutput(true, resolveOutputType(aType, bType));
+    };
+}
+
+function makeAsymmetricOnChange(
+    inputs: BinaryInputNames
+): (this: Blockly.Block, e: Blockly.Events.Abstract) => void {
+    return function (e) {
+        if (
+            e.type !== Blockly.Events.BLOCK_MOVE &&
+            e.type !== Blockly.Events.BLOCK_DRAG
+        ) return;
+        if (this.isInFlyout) return;
+
+        const [nameA, nameB] = inputs;
+        const aType = getConnectedType(this, nameA);
+        const bType = getConnectedType(this, nameB);
+
+        const aIsVec = aType && normalizeVec(aType) !== "Number";
+        const bIsVec = bType && normalizeVec(bType) !== "Number";
+
+        if (bIsVec) {
+            this.getInput(nameA)?.setCheck(withAlias(bType!));
+        } else {
+            this.getInput(nameA)?.setCheck(NUMERIC_TYPES);
+        }
+
+        if (aIsVec) {
+            this.getInput(nameB)?.setCheck([...withAlias(aType!), "Number"] as NumericGLSLType[]);
+        } else if (aType === "Number") {
+            this.getInput(nameB)?.setCheck(["Number"] as NumericGLSLType[]);
+        } else {
+            this.getInput(nameB)?.setCheck(NUMERIC_TYPES);
+        }
+
+        this.setOutput(true, resolveOutputType(aType, bType));
+    };
+}
+
+function makeMixOnChange(): (this: Blockly.Block, e: Blockly.Events.Abstract) => void {
+    return function (e) {
+        if (
+            e.type !== Blockly.Events.BLOCK_MOVE &&
+            e.type !== Blockly.Events.BLOCK_DRAG
+        ) return;
+        if (this.isInFlyout) return;
+
+        const aType = getConnectedType(this, "A");
+        const bType = getConnectedType(this, "B");
+
+        // A and B constrain each other symmetrically
+        this.getInput("A")?.setCheck(bType ? compatiblePair(bType) : NUMERIC_TYPES);
+        this.getInput("B")?.setCheck(aType ? compatiblePair(aType) : NUMERIC_TYPES);
+
+        // t (BY): float always works; same-size vec also works
+        const abType = aType ?? bType;
+        const tCheck: NumericGLSLType[] = abType && normalizeVec(abType) !== "Number"
+            ? ["Number", ...withAlias(abType)]
+            : NUMERIC_TYPES;
+        this.getInput("BY")?.setCheck(tCheck);
+
+        this.setOutput(true, resolveOutputType(aType, bType));
+    };
+}
+
 Blockly.Blocks["operators_add"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]);
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("+");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES);
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("+");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
-    }
+    },
+    onchange: makeSymmetricOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_subtract"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]);
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("-");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES);
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("-");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
-    }
+    },
+    onchange: makeSymmetricOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_multiply"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]);
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("*");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES);
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("*");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
-    }
+    },
+    onchange: makeSymmetricOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_divide"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]);
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("/");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES);
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("/");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
-    }
+    },
+    onchange: makeAsymmetricOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_power"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]);
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("^");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES);
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("^");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: makeAsymmetricOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_modulus"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]);
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("mod");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES);
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("mod");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: makeAsymmetricOnChange(["A", "B"]),
 };
+
+function makeUnaryOnChange(inputName: string): (this: Blockly.BlockSvg, e: Blockly.Events.Abstract) => void {
+    return function (e) {
+        if (
+            e.type !== Blockly.Events.BLOCK_MOVE &&
+            e.type !== Blockly.Events.BLOCK_DRAG
+        ) return;
+        if (this.isInFlyout) return;
+
+        const t = getConnectedType(this, inputName);
+        this.setOutput(true, t ? withAlias(t) : NUMERIC_TYPES);
+    };
+}
 
 Blockly.Blocks["operators_rounding"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("NUMBER").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField(
+        this.appendValueInput("NUMBER").setCheck(NUMERIC_TYPES).appendField(
             new Blockly.FieldDropdown(
                 [
                     ["round","round"],
@@ -155,16 +272,17 @@ Blockly.Blocks["operators_rounding"] = {
                 ]
             ),
             "OPERATION");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: makeUnaryOnChange("NUMBER"),
 };
 
 
 Blockly.Blocks["operators_trig"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("NUMBER").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField(
+        this.appendValueInput("NUMBER").setCheck(NUMERIC_TYPES).appendField(
             new Blockly.FieldDropdown(
                 [
                     ["sine of","sin"],
@@ -173,15 +291,16 @@ Blockly.Blocks["operators_trig"] = {
                 ]
             ),
             "OPERATION");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: makeUnaryOnChange("NUMBER"),
 };
 
-Blockly.Blocks["operators_unary"] = { // square root of, fractional of, negate, absolute value of, sign of, exp, log, exp2, log2
+Blockly.Blocks["operators_unary"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("NUMBER").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField(
+        this.appendValueInput("NUMBER").setCheck(NUMERIC_TYPES).appendField(
             new Blockly.FieldDropdown(
                 [
                     ["square root of", "sqrt"],
@@ -196,26 +315,28 @@ Blockly.Blocks["operators_unary"] = { // square root of, fractional of, negate, 
                 ]
             ),
             "OPERATION");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: makeUnaryOnChange("NUMBER"),
 };
 
 Blockly.Blocks["operators_mix"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("mix");
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("and");
-        this.appendValueInput("BY").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("by");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES).appendField("mix");
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("and");
+        this.appendValueInput("BY").setCheck(NUMERIC_TYPES).appendField("by");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: makeMixOnChange(),
 };
 
 Blockly.Blocks["operators_bounds"] = { // min and max
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("A").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField(
+        this.appendValueInput("A").setCheck(NUMERIC_TYPES).appendField(
             new Blockly.FieldDropdown(
                 [
                     ["max of", "max"],
@@ -223,22 +344,55 @@ Blockly.Blocks["operators_bounds"] = { // min and max
                 ]
             ),
             "OPERATION");
-        this.appendValueInput("B").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("and");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("B").setCheck(NUMERIC_TYPES).appendField("and");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: makeSymmetricOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_clamp"] = {
     init: function (this: Blockly.Block) {
         this.setInputsInline(true);
-        this.appendValueInput("NUMBER").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("clamp");
-        this.appendValueInput("MIN").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("between");
-        this.appendValueInput("MAX").setCheck(["Number", "Vector2", "Vector3", "Vector4", "Color"]).appendField("and");
-        this.setOutput(true, ["Number", "Vector2", "Vector3", "Vector4", "Color"]);
+        this.appendValueInput("NUMBER").setCheck(NUMERIC_TYPES).appendField("clamp");
+        this.appendValueInput("MIN").setCheck(NUMERIC_TYPES).appendField("between");
+        this.appendValueInput("MAX").setCheck(NUMERIC_TYPES).appendField("and");
+        this.setOutput(true, NUMERIC_TYPES);
         this.setStyle("operators_blocks");
     },
+    onchange: function (this: Blockly.Block, e: Blockly.Events.Abstract) {
+        if (
+            e.type !== Blockly.Events.BLOCK_MOVE &&
+            e.type !== Blockly.Events.BLOCK_DRAG
+        ) return;
+        if (this.isInFlyout) return;
+
+        const vType = getConnectedType(this, "NUMBER");
+        const bound = vType ? compatiblePair(vType) : NUMERIC_TYPES;
+        this.getInput("MIN")?.setCheck(bound);
+        this.getInput("MAX")?.setCheck(bound);
+        this.setOutput(true, vType ? withAlias(vType) : NUMERIC_TYPES);
+    },
 };
+
+function makeEqualityOnChange(
+    inputs: BinaryInputNames
+): (this: Blockly.Block, e: Blockly.Events.Abstract) => void {
+    return function (e) {
+        if (
+            e.type !== Blockly.Events.BLOCK_MOVE &&
+            e.type !== Blockly.Events.BLOCK_DRAG
+        ) return;
+        if (this.isInFlyout) return;
+
+        const [nameA, nameB] = inputs;
+        const aType = getConnectedType(this, nameA);
+        const bType = getConnectedType(this, nameB);
+
+        this.getInput(nameA)?.setCheck(bType ? withAlias(bType) : NUMERIC_TYPES);
+        this.getInput(nameB)?.setCheck(aType ? withAlias(aType) : NUMERIC_TYPES);
+    };
+}
 
 Blockly.Blocks["operators_equals"] = {
     init: function (this: Blockly.Block) {
@@ -248,6 +402,7 @@ Blockly.Blocks["operators_equals"] = {
         this.setOutput(true, "Boolean");
         this.setStyle("operators_blocks");
     },
+    onchange: makeEqualityOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_not_equals"] = {
@@ -258,6 +413,7 @@ Blockly.Blocks["operators_not_equals"] = {
         this.setOutput(true, "Boolean");
         this.setStyle("operators_blocks");
     },
+    onchange: makeEqualityOnChange(["A", "B"]),
 };
 
 Blockly.Blocks["operators_less_than"] = {
