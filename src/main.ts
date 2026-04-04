d3ae6408dad7b95c11024f1e22c49155b4e5eebb
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-04-03 18:31:40 -0600

    oh so THATS how that works

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+ import { mount } from 'svelte';
 import App from './ui/Editor.svelte';
 
-const app = App({
+const app = mount(App, {
   target: document.getElementById('app') || document.body,
 });
 
-export default app;
+export default app; 
