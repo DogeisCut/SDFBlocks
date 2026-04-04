ad5b08f007f4c79a2ac69d8138dff1ba5907d644
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-04-03 18:24:44 -0600

    really broken svelte setup

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import App from './ui/Editor.svelte';
+
+const app = App({
+  target: document.getElementById('app') || document.body,
+});
+
+export default app;
---
+++ src/ui/Editor.svelte
@@ -0,0 +1,1 @@
+<div style="padding:24px;font-family:system-ui, sans-serif">Svelte mount successful — static test</div>
